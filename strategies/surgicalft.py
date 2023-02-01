97ea8d85ed765c103a74d8ad3ce0a4508a6481d4
francesco-p <pelosinf@gmail.com>
2023-02-01 12:02:50 +0100

    adds surgical

--- strategies/surgicalft.py
+++ strategies/surgicalft.py
@@ -1,30 +1,34 @@
-from importlib.util import LazyLoader
-from pandas import set_option
 from opt import OPT
 import torch
-import copy
 import torch.nn.functional as F
-from torch import optim
 import torch.nn as nn
 import utils
+from torch import optim
+from strategies.base import Base
 
-class SurgicalFT():
-    """ https://arxiv.org/pdf/2210.11466.pdf """
+class SurgicalFT(Base):
+
+    """ https://arxiv.org/pdf/2210.11466.pdf 
+    Surgical works with models that have a layer X 
+    such as: model.layerX.parameters()
+    resnets are fine"""
 
     def __init__(self, model, layer) -> None:
-        self.layer = layer
+        super().__init__()
         self.model = model
+        self.layer = layer
         self.optimizer = self._set_optim(layer)
         self.loss_fn = nn.CrossEntropyLoss()
-        self.name = f"Surgical FT@layer{layer}"
+        self.name = "SurgicalFT"
+
 
     def train(self, train_loader, val_loader, writer, tag, scheduler=False):
         self.model.to(OPT.DEVICE)
 
         if scheduler:
-            sched = torch.optim.lr_scheduler.OneCycleLR(self.optimizer, 0.01, epochs=OPT.CONTINUAL_EPOCHS, steps_per_epoch=len(train_loader))
+            sched = torch.optim.lr_scheduler.OneCycleLR(self.optimizer, 0.01, epochs=OPT.EPOCHS_CONT, steps_per_epoch=len(train_loader))
 
-        for epoch in range(0, OPT.CONTINUAL_EPOCHS):
+        for epoch in range(0, OPT.EPOCHS_CONT):
             print(f'    EPOCH {epoch} ')
 
             ##################
@@ -42,11 +46,7 @@ class SurgicalFT():
                 y_hat = utils.check_output(self.model(x))['y_hat']
                 y_hat = y_hat.to(torch.float32)
                 y_onehot = F.one_hot(y, num_classes=OPT.NUM_CLASSES).to(torch.float32)
-
-                # Losses
-                l_c = self.loss_fn(y_hat, y_onehot)
-
-                loss_train = l_c
+                loss_train = self.loss_fn(y_hat, y_onehot)
 
                 # Backward
                 loss_train.backward()
@@ -58,56 +58,60 @@ class SurgicalFT():
                     sched.step()
 
                 # Compute measures
-                cumul_loss_train += l_c.item()
+                cumul_loss_train += loss_train.item()
                 cumul_acc_train += (y_hat.argmax(dim=1) == y).sum().item()
                 seen += len(y)
 
+            cumul_acc_train /= seen
+            cumul_loss_train /= seen
             # Print measures
-            self.log_metrics(seen, cumul_loss_train, cumul_acc_train, epoch, 'train', writer, tag)
+            self.log_metrics(cumul_loss_train, cumul_acc_train, epoch, 'train', writer, tag)
 
             ####################
             #### Validation ####
-            if (epoch == 0) or ((epoch % OPT.LOG_EVERY) == 0):
-                    with torch.no_grad():
-                        cumul_loss_val = 0
-                        cumul_acc_val = 0
-                        seen = 0
-                        self.model.eval()
-                        for x, y in val_loader:
-
-                            # Move to GPU
-                            x = x.to(OPT.DEVICE)
-                            y = y.to(OPT.DEVICE)
-
-                            # Forward to model
-                            y_hat = utils.check_output(self.model(x))['y_hat']
-                            y_hat = y_hat.to(torch.float32)
-                            y_onehot = F.one_hot(y, num_classes=OPT.NUM_CLASSES).to(torch.float32)
-
-                            l_c = self.loss_fn(y_hat, y_onehot)
-                            loss_train = l_c
-
-                            # Compute measures
-                            cumul_loss_val += l_c.item()
-                            cumul_acc_val += (y_hat.argmax(dim=1) == y).sum().item()
-                            seen += len(y)
-
-                        # Print measures
-                        self.log_metrics(seen, cumul_loss_val, cumul_acc_val, epoch, 'val', writer, tag)
-
-            # Save the model
-            if ((epoch % OPT.CHK_EVERY) == 0) and OPT.CHK:
-                torch.save(self.model.state_dict(), OPT.CHK_FOLDER+f'/{tag}_{epoch:04}_{OPT.MODEL}.pt')
+            if (epoch == 0) or ((epoch % OPT.EVAL_EVERY_CONT) == 0):
+                eval_loss, eval_acc = self.eval(val_loader, writer, tag)
+                #torch.save(self.model.state_dict(), OPT.CHK_FOLDER+f'/{tag}_{epoch:04}_{OPT.MODEL}.pt')
 
 
-    def log_metrics(self, seen, loss, acc, epoch, session, writer, tag):
+    def eval(self, val_loader, writer, tag):
+        """ Evaluate the model on the evaluation set"""
+        with torch.no_grad():
+            cumul_loss_eval = 0
+            cumul_acc_eval = 0
+            seen = 0
+            self.model.eval()
+            for x, y in val_loader:
+
+                # Move to GPU
+                x = x.to(OPT.DEVICE)
+                y = y.to(OPT.DEVICE)
+
+                # Forward to model
+                y_hat = utils.check_output(self.model(x))['y_hat']
+                y_hat = y_hat.to(torch.float32)
+                y_onehot = F.one_hot(y, num_classes=OPT.NUM_CLASSES).to(torch.float32)
+                loss_test = self.loss_fn(y_hat, y_onehot)
+
+                # Compute measures
+                cumul_loss_eval += loss_test.item()
+                cumul_acc_eval += (y_hat.argmax(dim=1) == y).sum().item()
+                seen += len(y)
+
+            cumul_acc_eval /= seen
+            cumul_loss_eval /= seen
+            # Print measures
+            self.log_metrics(cumul_loss_eval, cumul_acc_eval, 0, 'eval', writer, tag)
+        
+        return cumul_loss_eval, cumul_acc_eval
+
+
+    def log_metrics(self, loss, acc, epoch, session, writer, tag):
         """ Prints metrics to screen and logs to tensorboard """
-        loss /= seen
-        acc /= seen
-        print(f'        {session:<6} - l_c:{loss:.5f} a:{acc:.5f}')
+        print(f'        {tag}_{session:<6} - l:{loss:.5f}  a:{acc:.5f}')
 
-        if OPT.LOG:
-            writer.add_scalar(f'{tag}/loss_c/{session}', loss, epoch)
+        if OPT.TENSORBOARD:
+            writer.add_scalar(f'{tag}/loss/{session}', loss, epoch)
             writer.add_scalar(f'{tag}/acc/{session}', acc, epoch)
 
 
@@ -124,6 +128,6 @@ class SurgicalFT():
         elif layer == 4:
             tr_params = [{"params": self.model.fc.parameters()}]
 
-        optimizer = optim.Adam(tr_params,lr=OPT.CONTINUAL_LR, weight_decay=OPT.CONTINUAL_WD)
+        optimizer = optim.Adam(tr_params,lr=OPT.LR_CONT, weight_decay=OPT.WD_CONT)
 
         return optimizer
