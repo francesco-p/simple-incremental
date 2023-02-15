53a86b303dcb50eb27cebf23313a702f31999825
francesco-p <pelosinf@gmail.com>
2023-02-15 18:18:16 +0100

    adds stats normalization

--- stats.py
+++ stats.py
@@ -22,3 +22,12 @@ DSET_TRANSF = {
     "SVHN": trs.Compose([trs.RandomHorizontalFlip(),
                 trs.ToTensor()])
 }
+
+
+DSET_NORMALIZATION = {
+    "CIFAR10": trs.Normalize([0.4914, 0.4822, 0.4465], [0.2023, 0.1994, 0.2010]),
+    "CIFAR100": trs.Normalize([0.5071, 0.4867, 0.4408], [0.2675, 0.2565, 0.2761]),
+
+    # is it correct copilot suggested this?????
+    "SVHN": trs.Normalize([0.4377, 0.4438, 0.4728], [0.1980, 0.2010, 0.1970])
+}
